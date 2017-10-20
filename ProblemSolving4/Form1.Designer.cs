3bb29217ac3c7fd2ebed16d4eb095d72ed760ece
temp <temp>
2017-10-20 13:30:05 -0400

    Add project files.

---
+++ ProblemSolving4/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ProblemSolving4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContinueLabel = new System.Windows.Forms.Label();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ContinueLabel
+            // 
+            this.ContinueLabel.Font = new System.Drawing.Font("OCR A Std", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContinueLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ContinueLabel.Location = new System.Drawing.Point(190, 199);
+            this.ContinueLabel.Name = "ContinueLabel";
+            this.ContinueLabel.Size = new System.Drawing.Size(293, 62);
+            this.ContinueLabel.TabIndex = 0;
+            this.ContinueLabel.Text = "Click on Screen to Continue";
+            this.ContinueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // continueButton
+            // 
+            this.continueButton.Font = new System.Drawing.Font("Minion Pro", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.continueButton.Location = new System.Drawing.Point(290, 275);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(84, 29);
+            this.continueButton.TabIndex = 1;
+            this.continueButton.Text = "Continue?";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Visible = false;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(679, 506);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.ContinueLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ContinueLabel;
+        private System.Windows.Forms.Button continueButton;
+    }
+}
+
---
+++ ProblemSolving4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProblemSolving4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //screen click
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics wordString = CreateGraphics();
+            Font wordPen = new Font("Arial", 14, FontStyle.Bold);
+            SolidBrush wordBrush = new SolidBrush(Color.White);
+            ContinueLabel.Visible = false;
+            this.BackColor = Color.Black;
+            this.Refresh();
+            Thread.Sleep(3000);
+            continueButton.Visible = true;
+            wordString.DrawString("You", wordPen, wordBrush, 310, 250);
+            continueButton.Visible = true;
+        }
+        // button click
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            
+            Graphics wordString = CreateGraphics();
+            Font wordPen = new Font("Arial", 14, FontStyle.Bold);
+            SolidBrush wordBrush = new SolidBrush(Color.White);
+            wordString.Clear(Color.Black);
+            continueButton.Visible = false;
+            wordString.DrawString("you", wordPen, wordBrush, 100, 200);
+            Thread.Sleep(1000);
+            wordString.DrawString("should", wordPen, wordBrush, 150, 200);
+            Thread.Sleep(1000);
+            wordString.DrawString("not", wordPen, wordBrush, 230, 200);
+            Thread.Sleep(1000);
+            wordString.DrawString("have", wordPen, wordBrush, 280, 200);
+            Thread.Sleep(1000);
+            wordString.DrawString("done", wordPen, wordBrush, 340, 200);
+            Thread.Sleep(1000);
+            wordString.DrawString("that", wordPen, wordBrush, 400, 200);
+            Thread.Sleep(2500);
+            this.Close();
+        }
+    }
+}
